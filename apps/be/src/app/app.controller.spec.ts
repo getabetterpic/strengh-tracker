6e590ef63efb0d5da3a968d0cab0886a9a67c8af
Dan Smith <dan@getabetterpic.com>
2025-07-06 06:24:21 -0400

    Health check endpoint for app controller

--- apps/be/src/app/app.controller.spec.ts
+++ apps/be/src/app/app.controller.spec.ts
@@ -1,21 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DATABASE } from '@strength-tracker/db';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AppController', () => {
   let app: TestingModule;
+  let db;
 
   beforeAll(async () => {
+    db = {
+      execute: jest.fn().mockReturnValue({ rowCount: 2 }),
+    };
     app = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: DATABASE,
+          useValue: db,
+        },
+      ],
     }).compile();
   });
 
-  describe('getData', () => {
-    it('should return "Hello API"', () => {
+  describe('healthCheck', () => {
+    it('should return info about the connection', async () => {
       const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Hello API' });
+      const resp = await appController.healthCheck();
+      expect(resp).toEqual({ postgres: true });
+    });
+
+    describe('when Postgres is not working', () => {
+      it('returns a 400 bad request', async () => {
+        db.execute.mockRejectedValue('something went wrong');
+        const appController = app.get<AppController>(AppController);
+        try {
+          await appController.healthCheck();
+          expect(true).toEqual(false); // should not get here
+        } catch (e) {
+          expect(e).toEqual(new BadRequestException({ postgres: false }));
+        }
+      });
     });
   });
 });
--- apps/be/src/app/app.controller.ts
+++ apps/be/src/app/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Inject,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { DATABASE } from '@strength-tracker/db';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(@Inject(DATABASE) private readonly db: NodePgDatabase) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @Get('health-check')
+  async healthCheck() {
+    try {
+      const result = await this.db.execute('SELECT 1=1;');
+      return { postgres: !!result.rowCount };
+    } catch (e) {
+      const error =
+        process.env.NODE_ENV === 'production'
+          ? 'The database could not connect.'
+          : e.message;
+      throw new InternalServerErrorException({
+        postgres: false,
+        error,
+      });
+    }
   }
 }
