6e14ce3a73aeec609cc8fdda18ab859aacca3b4f
Dan <dan@getabetterpic.com>
2025-08-23 06:45:52 -0400

    Use host/port instead of URL for connecting to Postgres

--- libs/db/src/lib/db.module.ts
+++ libs/db/src/lib/db.module.ts
@@ -7,7 +7,19 @@ export const DATABASE = 'DATABASE';
   providers: [
     {
       provide: DATABASE,
-      useFactory: () => drizzle(process.env['DATABASE_URL'] as string),
+      useFactory: () =>
+        drizzle({
+          connection: {
+            host: process.env['DATABASE_HOST'],
+            port: parseInt(process.env['DATABASE_PORT'] || ''),
+            user: process.env['DATABASE_USER'],
+            password: process.env['DATABASE_PASSWORD'],
+            database: process.env['DATABASE_NAME'],
+            ssl: {
+              ca: process.env['DATABASE_CA'],
+            },
+          },
+        }),
     },
   ],
   exports: [DATABASE],
